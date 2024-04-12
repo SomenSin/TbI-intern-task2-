563bbcd998660bca26cbf46d3620527b7bcb2628
Somendra singh <93323212+SomenSin@users.noreply.github.com>
2024-04-13 00:35:59 +0530

    Update merge-strings-alternately.cpp

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     string mergeAlternately(string w1, string w2) {
-        int s1=w1.size(),s2=w2.size();
-        int i=0,j=0,x=0;
-        string ans="";
-        while(i<s1 && j<s2) {
-            if(x%2 == 0) {
-                ans += w1[i++];
+        int s1 = w1.size(), s2 = w2.size(); // Get sizes of both strings
+        int i = 0, j = 0, x = 0; // Initialize iterators and counter
+        string ans = ""; // Initialize result string
+        
+        // Iterate through both strings alternately
+        while (i < s1 && j < s2) {
+            if (x % 2 == 0) { // If counter is even
+                ans += w1[i++]; // Append character from w1 to result and increment w1 index
+            } else {
+                ans += w2[j++]; // Append character from w2 to result and increment w2 index
             }
-            else {
-                ans += w2[j++];
-            }
-            x++;
+            x++; // Increment counter
         }
-        while(i<s1) {
-            ans += w1[i++]; 
+        
+        // Append remaining characters from w1 if any
+        while (i < s1) {
+            ans += w1[i++];
         }
-        while(j<s2) {
+        
+        // Append remaining characters from w2 if any
+        while (j < s2) {
             ans += w2[j++];
         }
 
-        return ans;
+        return ans; // Return the merged string
     }
 };
