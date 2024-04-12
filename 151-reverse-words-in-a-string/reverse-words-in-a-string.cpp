34b29872a8a2383d32fe5330ba2401f77483f378
Somendra singh <93323212+SomenSin@users.noreply.github.com>
2024-04-13 00:35:28 +0530

    Update reverse-words-in-a-string.cpp

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stringstream ss(s);
-        string word,ans="";
-        while(ss >> word) {
-            ans = word + " " + ans;
+        stringstream ss(s); // Create a stringstream object to tokenize the input string
+        string word, ans=""; // Initialize variables to store each word and the reversed string
+
+        // Tokenize the input string by extracting words one by one
+        while (ss >> word) {
+            ans = word + " " + ans; // Concatenate each word to the beginning of the result string
         }
-        ans.pop_back();
-        return ans;
+        ans.pop_back(); // Remove the extra space at the end of the result string
+        return ans; // Return the reversed string
     }
 };
